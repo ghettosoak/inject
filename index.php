7fa20d3b84d45f5b957375cb55060c02c2ee51bb
Mike <ghettosoak@gmail.com>
2014-03-29 11:09:31 +0100

    The calm before the storm
    Fun with favicon, and a few smallish style changes. Nice!

--- index.php
+++ index.php
@@ -20,6 +20,10 @@ while ($theavailablity = mysql_fetch_array($available, MYSQL_ASSOC)){
     <title>in / ject</title>
     <meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1" />
     <meta name="description" content="Thoughts and musings of a designer pretending to be a developer; and an American pretending to be a Swiss." />
+
+    <link rel="shortcut icon" href="img/favicon.ico" />
+    <link rel="apple-touch-icon" href="img/apple-touch-icon.png" />
+        
     <link rel="stylesheet" type="text/css" href="css/master.css" />
     <script>
     	<?php 
@@ -69,5 +73,18 @@ while ($theavailablity = mysql_fetch_array($available, MYSQL_ASSOC)){
 	</div>
 
 <script type="text/javascript" src="js/master-m.js"></script>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-31902149-2', 'ject.ch');
+  ga('send', 'pageview', {
+   'page': location.pathname + location.search  + location.hash
+  });
+  
+</script>
 </body>
 </html>
